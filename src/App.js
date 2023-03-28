99a4a96298d1e37e262411e55739f63fbf4b9dc9
Monte Honarchian <montehonarchian@gmail.com>
2023-03-27 18:42:47 -0700

    Removed Cards import

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './App.css';
-import Cards from './components/Cards';
+
+import Footer from './components/Footer';
 import Header from './components/Header';
 import Home from './components/Home';
-import Footer from './components/Footer';
 
 function App() {
   return (
